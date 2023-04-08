8efe00944677928e8c652c902814a9c3576595e9
Lucas <lucasx131@gmail.com>
2023-04-08 13:55:59 +0200

    Finalize single vod download

--- dlp.py
+++ dlp.py
@@ -1,7 +1,22 @@
 import pyperclip
+import subprocess
+import os
+import sys
+import lib
 
 
-link = pyperclip.paste()
-print(link)
+work_dir = r'F:\Downloads\yt-dlp'
+dest_dir = r'D:\Downloads\yt-dlp'
 
-# comment
+
+def main():
+    link = pyperclip.paste()
+    os.chdir(work_dir)
+    subprocess.run([sys.executable, '-m', 'yt_dlp', link])
+    try:
+        lib.move_file(work_dir, dest_dir)
+    except ValueError:
+        lib.exit()
+
+
+main()
