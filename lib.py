221bc644acbd7d0332e2d6c9ec1cded4bea20539
Lucas <lucasx131@gmail.com>
2023-06-28 19:18:47 +0200

    adjust regex to exclude jp strings

--- lib.py
+++ lib.py
@@ -26,3 +26,9 @@ def move_file(work_dir, dest_dir):
         shutil.copytree(src_folder, dest_folder, dirs_exist_ok=True)
         shutil.rmtree(src_folder)
 
+
+def get_id2(name):
+    re_id = re.compile(r"(?<=\()((?![\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FFF])[^\"&?\/\s]){11}(?=\))")
+    ytid = re_id.search(name)
+    return ytid
+
